f06b4c3a42001cf2f5e660e012c60f77fc65a0c6
FlooyJin <jinsungpark333@gmail.com>
2022-02-02 11:40:20 -0800

    test

--- generator.py
+++ generator.py
@@ -1,1 +1,13 @@
-print("hello world!")
+import sys
+sys.path.insert(1, './firrtl-operations')
+from my_uint import *
+
+a = my_uint(2, 0x2)
+b = my_uint(4, 0xa)
+sum = a.uint_add(b)
+
+def gen():
+    print("test", sum.value)
+
+if __name__=="__main__":
+    gen()
