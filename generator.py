e78d47a59f8dbdbbc2a4f3800dd1f50298472847
FlooyJin <jinsungpark333@gmail.com>
2022-02-08 10:32:09 -0800

    subtracting by same variable causes assert error

--- generator.py
+++ generator.py
@@ -6,8 +6,66 @@ a = my_uint(2, 0x2)
 b = my_uint(4, 0xa)
 sum = a.uint_add(b)
 
-def gen():
-    print("test", sum.value)
+def header(f):
+    f.write("#include \"./firrtl-sig/uint.h\"\n")
+    f.write("#include<iostream>\n")
+    f.write("#include <assert.h>\n")
+    f.write("using namespace std;\n")
+
+def middle(f):
+
+    a = my_uint(16, 0xcafe)
+    b = my_uint(16, 0xbebe)
+    c = my_uint(64, 0xe2bd5b4ff8b30fc8)
+    d = my_uint(64, 0x2fc353e33c6938a7)
+
+    li = []
+    li.append(a)
+    li.append(b)
+    li.append(c)
+    li.append(d)
+
+    f.write("int main() {\n")
+    
+    for count, num in enumerate(li):
+        f.write("\tUInt<" + str(num.bitsize) + "> u" + str(count) + "(\"" + str(hex(num.value)) + "\");\n")
+
+    f.write("\n")
+
+    for ia, aval in enumerate(li):
+        for ib, bval in enumerate(li):
+            if(aval.bitsize == bval.bitsize):
+                result = aval.uint_add(bval)
+                f.write("\tassert(u"+str(ia)+"+u"+str(ib)+" == UInt<" + str(result.bitsize) + ">(\"" + str(hex(result.value)) + "\"));\n")
+
+    f.write("\n")
+
+    for ia, aval in enumerate(li):
+        for ib, bval in enumerate(li):
+            if(aval.bitsize == bval.bitsize):
+                result = aval.uint_sub(bval)
+                f.write("\tassert(u"+str(ia)+"-u"+str(ib)+" == UInt<" + str(result.bitsize) + ">(\"" + str(hex(result.value)) + "\"));\n")
+
+
+    #f.write("UInt<16>  a16u("+str(hex(a.value))+ ");\n")
+    #f.write("UInt<16>  b16u("+str(hex(b.value))+ ");\n")
+    #f.write("UInt<64>  a64u("+str(hex(c.value))+ ");\n")
+    #f.write("UInt<64>  b64u("+str(hex(d.value))+ ");\n")
+
+    #result1 = a.uint_add(b)
+    #result2 = c.uint_add(d)
+
+    #f.write("assert(a16u + b16u == UInt<" + str(result1.bitsize) + ">(\"" + str(hex(result1.value)) + "\"));\n")
+    #f.write("assert(a64u + b64u == UInt<" + str(result2.bitsize) + ">(\"" + str(hex(result2.value)) + "\"));\n")
+
+    f.write("\treturn 0;\n")
+    f.write("}")
 
 if __name__=="__main__":
-    gen()
+
+    f = open("runner.cpp", "w")
+
+    header(f)
+    middle(f)
+
+    f.close()
---
+++ runner.cpp
@@ -0,0 +1,30 @@
+#include "./firrtl-sig/uint.h"
+#include<iostream>
+#include <assert.h>
+using namespace std;
+int main() {
+	UInt<16> u0("0xcafe");
+	UInt<16> u1("0xbebe");
+	UInt<64> u2("0xe2bd5b4ff8b30fc8");
+	UInt<64> u3("0x2fc353e33c6938a7");
+
+	assert(u0+u0 == UInt<17>("0x195fc"));
+	assert(u0+u1 == UInt<17>("0x189bc"));
+	assert(u1+u0 == UInt<17>("0x189bc"));
+	assert(u1+u1 == UInt<17>("0x17d7c"));
+	assert(u2+u2 == UInt<65>("0x1c57ab69ff1661f90"));
+	assert(u2+u3 == UInt<65>("0x11280af33351c486f"));
+	assert(u3+u2 == UInt<65>("0x11280af33351c486f"));
+	assert(u3+u3 == UInt<65>("0x5f86a7c678d2714e"));
+
+	assert(u0-u0 == UInt<17>("0x0"));
+	assert(u0-u1 == UInt<17>("0xc40"));
+	assert(u1-u0 == UInt<17>("0x1f3c0"));
+	assert(u1-u1 == UInt<17>("0x0"));
+	//assert(u2-u2 == UInt<65>("0x0"));
+	assert(u2-u3 == UInt<65>("0xb2fa076cbc49d721"));
+	assert(u3-u2 == UInt<65>("0x14d05f89343b628df"));
+	//assert(u3-u3 == UInt<65>("0x0"));
+
+	return 0;
+}
