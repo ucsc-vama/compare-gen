a355379faad163ef9ce80b950305ebf6aebfb81d
FlooyJin <jinsungpark333@gmail.com>
2022-03-05 20:57:19 -0800

    simplify

--- Runner.cpp
+++ Runner.cpp
@@ -5,23 +5,20 @@
 using namespace std;
 
 int main() {
-	UInt<16> u0("0xcafe");
-	UInt<16> u1("0xbebe");
-	UInt<64> u2("0xe2bd5b4ff8b30fc8");
-	UInt<64> u3("0x2fc353e33c6938a7");
 
-	assert(u0+u1 == UInt<17>("0x189bc"));
-	assert(u0-u1 == UInt<17>("0xc40"));
-	assert(u0*u1 == UInt<32>("0x973f2c84"));
-	assert(u0/u1 == UInt<16>("0x1"));
-	assert(u0%u1 == UInt<16>("0xc40"));
-
-	assert(0==(u2<u3));
-	assert(0==(u2<=u3));
-	assert(1==(u2>u3));
-	assert(1==(u2>=u3));
-	assert(0==(u2==u3));
-	assert(1==(u2!=u3));
+	UInt<16> ua("0xcafe");
+	UInt<16> ub("0xbebe");
+	assert(ua+ub == UInt<17>("0x189bc"));
+	assert(ua-ub == UInt<17>("0xc40"));
+	assert(ua*ub == UInt<32>("0x973f2c84"));
+	assert(ua/ub == UInt<16>("0x1"));
+	assert(ua%ub == UInt<16>("0xc40"));
+	assert(0 == (ua<ub));
+	assert(0 == (ua<=ub));
+	assert(1 == (ua>ub));
+	assert(1 == (ua>=ub));
+	assert(0 == (ua==ub));
+	assert(1 == (ua!=ub));
 
 	return 0;
 }
--- generator.py
+++ generator.py
@@ -3,46 +3,6 @@ sys.path.insert(1, './firrtl-operations')
 from my_uint import *
 
 class operation:
-    def declareVariable(f, name, num):
-        f.write("\tUInt<" + str(num.bitsize) + "> u" + name + "(\"" + str(hex(num.value)) + "\");\n")
-
-    def binary(f, op, ops, li, a, b):
-        func = ops[op]
-        result = func(a,b)
-        f.write("\tassert(u"+str(li.index(a))+op+"u"+str(li.index(b))+" == UInt<"+str(result.bitsize)+">(\"" + str(hex(result.value)) + "\"));\n")
-
-    def unary(f, op, ops, li, a, b):
-        func = ops[op]
-        result = func(a,b)
-        f.write("\tassert("+str(result.value) +"==(u"+str(li.index(a))+op+"u"+str(li.index(b))+"));\n")
-
-def header(f):
-    f.write("#include \"./firrtl-sig/uint.h\"\n")
-    f.write("#include<iostream>\n")
-    f.write("#include <assert.h>\n")
-    f.write("#include <stdlib.h>\n")
-    f.write("using namespace std;\n\n")
-
-def middle(f):
-
-    a = my_uint(16, 0xcafe)
-    b = my_uint(16, 0xbebe)
-    c = my_uint(64, 0xe2bd5b4ff8b30fc8)
-    d = my_uint(64, 0x2fc353e33c6938a7)
-
-    li = []
-    li.append(a)
-    li.append(b)
-    li.append(c)
-    li.append(d)
-
-    f.write("int main() {\n")
-    
-    #print test variables
-    for count, num in enumerate(li):
-        operation.declareVariable(f, str(count), num)
-    f.write("\n")
-
     ops = {}
     ops["+"] = my_uint.uint_add
     ops["-"] = my_uint.uint_sub
@@ -56,29 +16,57 @@ def middle(f):
     ops["=="] = my_uint.uint_eq
     ops["!="] = my_uint.uint_neq
 
-    operation.binary(f,"+",ops,li,a,b)
-    operation.binary(f,"-",ops,li,a,b)
-    operation.binary(f,"*",ops,li,a,b)
-    operation.binary(f,"/",ops,li,a,b)
-    operation.binary(f,"%",ops,li,a,b)
+    def declareVariable(f, num):
+        f.write("\tUInt<" + str(num[1].bitsize) + "> u" + num[0] + "(\"" + str(hex(num[1].value)) + "\");\n")
 
-    f.write("\n")
-    operation.unary(f,"<",ops,li,c,d)
-    operation.unary(f,"<=",ops,li,c,d)
-    operation.unary(f,">",ops,li,c,d)
-    operation.unary(f,">=",ops,li,c,d)
-    operation.unary(f,"==",ops,li,c,d)
-    operation.unary(f,"!=",ops,li,c,d)
+    def binary(f, op, a, b):
+        func = operation.ops[op]
+        result = func(a[1],b[1])
+        f.write("\tassert(u"+a[0]+op+"u"+b[0]+" == UInt<"+str(result.bitsize)+">(\"" + str(hex(result.value)) + "\"));\n")
+
+    def unary(f, op, a, b):
+        func = operation.ops[op]
+        result = func(a[1],b[1])
+        f.write("\tassert("+str(result.value) +" == (u"+a[0]+op+"u"+b[0]+"));\n")
+
+def top(f):
+    f.write("#include \"./firrtl-sig/uint.h\"\n")
+    f.write("#include<iostream>\n")
+    f.write("#include <assert.h>\n")
+    f.write("#include <stdlib.h>\n")
+    f.write("using namespace std;\n\n")
+    f.write("int main() {\n\n")
+
+def testcase(f):
+    top(f)
+    a = ("a", my_uint(16, 0xcafe))
+    b = ("b", my_uint(16, 0xbebe))
+    operation.declareVariable(f, a)
+    operation.declareVariable(f, b)
+    operation.binary(f,"+",a,b)
+    operation.binary(f,"-",a,b)
+    operation.binary(f,"*",a,b)
+    operation.binary(f,"/",a,b)
+    operation.binary(f,"%",a,b)
+    operation.unary(f,"<",a,b)
+    operation.unary(f,"<=",a,b)
+    operation.unary(f,">",a,b)
+    operation.unary(f,">=",a,b)
+    operation.unary(f,"==",a,b)
+    operation.unary(f,"!=",a,b)
+    bottom(f)
     
+def bottom(f):
     f.write("\n")
     f.write("\treturn 0;\n")
     f.write("}")
 
 if __name__=="__main__":
 
-    f = open("Runner.cpp", "w")
+    f = open("Runner1.cpp", "w")
+    testcase(f)
 
-    header(f)
-    middle(f)
+    f = open("Runner2.cpp", "w")
+    testcase(f)
 
     f.close()
