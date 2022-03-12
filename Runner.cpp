eb9d1d684a60e835d3159edbcd6e75f445741d50
FlooyJin <jinsungpark333@gmail.com>
2022-03-11 22:25:29 -0800

    example

--- Runner.cpp
+++ Runner.cpp
@@ -9,19 +9,7 @@ int main() {
 	UInt<16> ua("0xcafe");
 	UInt<16> ub("0xbebe");
 	assert(ua+ub == UInt<17>("0x189bc"));
-	assert(ua-ub == UInt<17>("0xc40"));
-	assert(ua*ub == UInt<32>("0x973f2c84"));
-	assert(ua/ub == UInt<16>("0x1"));
-	assert(ua%ub == UInt<16>("0xc40"));
-	assert(0 == (ua<ub));
-	assert(0 == (ua<=ub));
-	assert(1 == (ua>ub));
-	assert(1 == (ua>=ub));
 	assert(0 == (ua==ub));
-	assert(1 == (ua!=ub));
-
-	assert(ua.pad<4>() == UInt<16>("0xcafe"));
-	assert(ua.shl<4>() == UInt<20>("0xcafe0"));
 	assert(ua.shr<4>() == UInt<12>("0xcaf"));
 
 	return 0;
