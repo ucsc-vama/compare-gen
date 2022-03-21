3e08b3a2cd23e26525564497a48506950cc9922b
FlooyJin <jinsungpark333@gmail.com>
2022-03-21 12:25:08 -0700

    comments and removed makefile

--- Runner.cpp
+++ Runner.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 int main() {
 
-	UInt<16> u59046("0xe6a6");
-	assert(u59046.shr<4>() == UInt<12>("0xe6a"));
+	UInt<16> u53127("0xcf87");
+	UInt<16> u55840("0xda20");
+	assert(u53127+u55840 == UInt<17>("0x1a9a7"));
 
 	return 0;
 }
--- generator.py
+++ generator.py
@@ -22,11 +22,11 @@ class operation:
     ops["shl"] = my_uint.uint_shl
     ops["shr"] = my_uint.uint_shr
 
-    def randcreate(digitlength):
+    def randcreate(digitlength: int) -> int:
         return randint(2**(digitlength-1), (2**digitlength)-1)
 
-    def declareVariable(f, num, name):
-        f.write("\tUInt<" + str(num.bitsize) + "> u" + name + "(\"" + str(hex(num.value)) + "\");\n")
+    def declareVariable(f, num: my_uint):
+        f.write("\tUInt<" + str(num.bitsize) + "> u" + str(num.value) + "(\"" + str(hex(num.value)) + "\");\n")
 
     def genvariables(f):
         bitsize = 16
@@ -37,33 +37,33 @@ class operation:
         operation.declareVariable(f, li[1], "b")
         return li
 
-    def binary(f, op, a, b):
+    def binary(f, op: str, a: my_uint, b: my_uint):
         func = operation.ops[op]
         result = func(a,b)
         f.write("\tassert(u"+str(a.value)+op+"u"+str(b.value)+ " == UInt<"+str(result.bitsize)+">(\"" + str(hex(result.value)) + "\"));\n")
 
-    def unary(f, op, a, b):
+    def unary(f, op: str, a: my_uint, b: my_uint):
         func = operation.ops[op]
         result = func(a,b)
         f.write("\tassert("+str(result.value) +" == (u"+str(a.value)+op+"u"+str(b.value)+"));\n")
 
-    def bitwise(f, op, a, n):
+    def bitwise(f, op: str, a: my_uint, n: int):
         func = operation.ops[op]
         result = func(a, n)
         f.write("\tassert(u"+str(a.value)+"."+op+"<"+str(n)+">() == UInt<"+str(result.bitsize)+">(\""+str(hex(result.value))+"\"));\n")
 
 class file:
-    def __init__(self, testcase):
+    def __init__(self, testcase): #initalize file
         f = open("Runner.cpp", "w")
         self.f = f
         self.testcase = testcase
 
-    def new_uint(self, bitsize, value):
+    def new_uint(self, bitsize: int, value: int) -> my_uint:# create my_uint and declare in cpp
         obj = my_uint(bitsize, value)
-        operation.declareVariable(self.f, obj, str(value))
+        operation.declareVariable(self.f, obj)
         return obj
 
-    def docalculate(self, op, a ,b):
+    def docalculate(self, op: str, a: my_uint ,b: my_uint): #call correct operation
         bins = ["+", "-", "*", "/", "%"]
         uns = ["<", "<=", ">", ">=", "==", "!="]
         bits = ["pad", "shl", "shr"]
@@ -74,7 +74,7 @@ class file:
         elif op in bits:
             operation.bitwise(self.f, op, a, b)
 
-    def top(self):
+    def top(self):#header and main
         self.f.write("#include \"./firrtl-sig/uint.h\"\n")
         self.f.write("#include <iostream>\n")
         self.f.write("#include <assert.h>\n")
@@ -82,34 +82,34 @@ class file:
         self.f.write("using namespace std;\n\n")
         self.f.write("int main() {\n\n")
 
-    def bottom(self):
+    def bottom(self): #return and closing
         self.f.write("\n")
         self.f.write("\treturn 0;\n")
         self.f.write("}")
 
     def runprogram(self):
         self.top()
-        self.testcase(self)
+        self.testcase(self) # run test
         self.bottom()
         self.f.close()
-        subprocess.call("make")
-        subprocess.call("./Runner")
-        subprocess.call(["make", "clean"])
+        subprocess.call(["g++", "Runner.cpp", "-o", "output"]) #test cpp program
+        subprocess.call(["./output"])
+        subprocess.call(["rm", "output"])
 
-    def testcase1(self):
+    def testcase1(self): # modify to test for given variable
         a = self.new_uint(16, operation.randcreate(16))
         b = self.new_uint(16, operation.randcreate(16))
         self.docalculate("+", a, b)
 
-    def testcase2(self):
+    def testcase2(self): #another example
         a = self.new_uint(16, operation.randcreate(16))
         self.docalculate("shr", a, 4)
 
 if __name__=="__main__":
-    subprocess.call(["make", "clean"])
+    subprocess.call(["rm", "Runner.cpp"])
     test = file(file.testcase1)
     test.runprogram()
-    test = file(file.testcase2)
-    test.runprogram()
+    #test = file(file.testcase2)
+    #test.runprogram()
     #subprocess.call(["rm", "Runner.cpp"])
 
