25c35251fdb73186293ac41a9c4887f142a838d5
FlooyJin <jinsungpark333@gmail.com>
2022-04-13 00:00:04 -0700

    yes

--- test.py
+++ test.py
@@ -1,23 +1,48 @@
 import subprocess
 import generator
 
-def populate_list(self, n, bitsize):
-    li = []
+def populate_list(self, li, n, bitsize):
     for i in range(n):
         li.append(generator.file.new_uint(self, bitsize, generator.operation.randcreate(bitsize)))
     return li
 
-def loop_calc(self, li):
+def calc_all(self, li):
     for i in li:
         for j in li:
             generator.file.docalculate(self,"+", i, j)
+            generator.file.docalculate(self,"-", i, j)
+            generator.file.docalculate(self,"*", i, j)
+            if j.bitsize < 64 and j.value > 0:
+                generator.file.docalculate(self,"/", i, j)
+                generator.file.docalculate(self,"%", i, j)
 
+def calc_all_64(self, li):
+    for i in li:
+        for j in li:
+            generator.file.docalculate(self,"+", i, j)
+            generator.file.docalculate(self,"-", i, j)
+            generator.file.docalculate(self,"*", i, j)
+            
 def testcase3(self):
-    li = populate_list(self, 3, 16)
-    loop_calc(self, li)
+    #simple random cases
+    # li = []
+    # li = populate_list(self, li, 3, 16)
+    # calc_all(self, li)
 
-    li = populate_list(self, 3, 64)
-    loop_calc(self, li)
+    # li = []
+    # li = populate_list(self, li, 3, 127)
+    # calc_all(self, li)
+
+    #custom cases
+    li = []
+    li.append(generator.file.new_uint(self, 1, 0x0))
+    #li.append(generator.file.new_uint(self, 1, 0x1))
+    li.append(generator.file.new_uint(self, 2, 0x2))
+    #li.append(generator.file.new_uint(self, 2, 0x3))
+    #li.append(generator.file.new_uint(self, 5, 0x11))
+    calc_all(self, li)
+
+    
 
 if __name__=="__main__":
     subprocess.call(["rm", "Runner.cpp"])
