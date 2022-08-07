9e2270c21e44b8f5305800f649604df963e29b71
floofyjin <jinsungpark333@gmail.com>
2022-08-07 15:34:07 -0700

    better test name

--- test.py
+++ test.py
@@ -66,10 +66,10 @@ class runtests:
         subprocess.call(["mkdir", "testcases/"+str(self.ts)+"/random"])
 
     def testpossible(self, varsize):
-        # for op in list_two:
-        #     for i in range(0,varsize): #testing only after 8. change after fix
-        #         for j in range(0,varsize):
-        #             self.calc_variables(str(self.ts)+"/brute","test"+ str(i) + ''.join(str(ord(c)) for c in op) + str(j), op, i, j)
+        for op in list_two:
+            for i in range(0,varsize): #testing only after 8. change after fix
+                for j in range(0,varsize):
+                    self.calc_variables(str(self.ts)+"/brute","test"+ str(i) + "_" + ''.join(str(ord(c)) for c in op) + "_" + str(j), op, i, j)
         # for op in list_bitwise:
         #     for i in range(0,varsize):
         #         for j in range(5):
@@ -77,20 +77,20 @@ class runtests:
         # for op in headtail:
         #     for i in range(0,varsize):
         #         for j in range(0,i+1):
-        #             self.calc_variables(str(self.ts)+"/brute","test"+ str(i) + ''.join(str(ord(c)) for c in op) + str(j), op, i, j)
+        #             self.calc_variables(str(self.ts)+"/brute","test"+ str(i) + "_" + op + "_" + str(j), op, i, j)
         # for op in binbit:
         #     for i in range(0,varsize):
-        #         self.calc_variables(str(self.ts)+"/brute","test"+ str(i) + ''.join(str(ord(c)) for c in op), op, i)
+        #         self.calc_variables(str(self.ts)+"/brute","test"+ op + "_" + str(i), op, i)
         # for op in sins:
         #     for i in range(0, varsize):
         #         self.calc_variables(str(self.ts)+"/brute","test"+ "~_"+str(i), op, i)
-        for op in threeparm:
-            for i in range(1, varsize):
-                isize = getbitsize(i)
-                for h in range(0, isize):
-                    hsize = getbitsize(h)
-                    for l in range(0, hsize):
-                        self.calc_variables(str(self.ts)+"/brute","test"+ "bits_"+str(i)+"_"+str(h)+"_"+str(l), op, i,h,l)
+        # for op in threeparm:
+        #     for i in range(1, varsize):
+        #         isize = getbitsize(i)
+        #         for h in range(1, isize):
+        #             hsize = getbitsize(h)
+        #             for l in range(1, hsize):
+        #                 self.calc_variables(str(self.ts)+"/brute","test"+ "bits_"+str(i)+"_"+str(h)+"_"+str(l), op, i,h,l)
 
     def testsquare(self, varsize):
         for op in list_two:
@@ -125,7 +125,7 @@ class runtests:
 
 if __name__=="__main__":
     a = runtests()
-    a.testcasemanual()
-    # a.testcasebrute(3)
+    # a.testcasemanual()
+    a.testcasebrute(3)
     # a.testcaserandom(4)
     a.printresult()
